b7c9a3de2d74228c1c6d4be0746047d0d848df41
gizemmm <119964833+gzmaydn08@users.noreply.github.com>
2023-01-24 21:40:33 +0300

    Add files via upload
    Signed-off-by: gizemmm <119964833+gzmaydn08@users.noreply.github.com>

---
+++ Main.java
@@ -0,0 +1,22 @@
+package paketler;
+
+
+import org.w3c.dom.Node;
+import javax.swing.*;
+
+
+public class Main {
+
+    private JFrame frame;
+
+
+    public static void main(String[] args) {
+
+
+        Node node1 = null;
+        org.w3c.dom.Node node2 = null;
+
+
+
+    }
+}
