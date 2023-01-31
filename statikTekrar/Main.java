167a1e6aa42febcc5334e65a0e0d8a96a5b76aee
gizemmm <119964833+gzmaydn08@users.noreply.github.com>
2023-01-31 21:00:51 +0300

    Add files via upload
    Signed-off-by: gizemmm <119964833+gzmaydn08@users.noreply.github.com>

---
+++ statikTekrar/Main.java
@@ -0,0 +1,19 @@
+package statikTekrar;
+
+public class Main {
+    public static void main(String[] args) {
+       /* StaticClass s1 = new StaticClass("gizo");
+        StaticClass s2 = new StaticClass("ayşo");
+
+        System.out.println("obje sayısı : "+StaticClass.objeSayisi);
+        System.out.println("obje sayısı : "+s1.objeSayisi);
+        System.out.println("obje sayısı : "+s2.objeSayisi);
+*/
+        toplama(3,4,5);
+
+
+    }
+    public static void toplama(int a, int b ,int c){
+        System.out.println(a+b+c);
+    }
+}
---
+++ statikTekrar/StaticClass.java
@@ -0,0 +1,14 @@
+package statikTekrar;
+
+public class StaticClass {
+    public  static  int objeSayisi= 0;
+    private static String isim;
+    public StaticClass (String isim){
+        this.isim=isim;
+        objeSayisi++;
+
+    }
+    public static String getIsim() {
+        return isim;
+    }
+}
