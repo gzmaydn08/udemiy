8f41d7aeb252da7b109b4da5ace55d00f082746c
gizemmm <119964833+gzmaydn08@users.noreply.github.com>
2023-01-31 21:31:12 +0300

    Add files via upload
    Signed-off-by: gizemmm <119964833+gzmaydn08@users.noreply.github.com>

---
+++ interface3/IDeneme.java
@@ -0,0 +1,16 @@
+package interface3;
+
+public interface IDeneme {
+    public int a = 4;
+   // private int b =  private kullanılmazz interface de
+
+    //public int b; = kullanman gerekir
+
+    public String deneme = "deneme";
+    public final int c =6;
+
+    public static void deneme (){
+        System.out.println("deneme");
+    }
+
+}
